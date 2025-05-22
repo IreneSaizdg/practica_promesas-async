f3aa568fe813b4e7c78d7d4f91550e5bf6adee2d
IreneSaizdg <irenesaiz@idarte.eus>
2025-05-22 12:01:35 +0200

    ADD: basic event

--- js/main.js
+++ js/main.js
@@ -1,15 +1,5 @@
 /*
 PSEUDO-CÓDIGO
--------------------------------------------------------->
-VARIABLES:
-Llamada al DOM
-
--------------------------------------------------------->
-EVENTOS:
-Evento click en botón -> pinta card
-
--------------------------------------------------------->
-FUNCIONES:
 Entrada: API
 const api = () => {
     //Hacer nueva promesa  
@@ -26,7 +16,51 @@ const consultaApi = () =>{
         //.then -> resolve
         //.catch -> reject
 }
-
--------------------------------------------------------->
-INVOCACIONES:
 */
+
+
+
+
+// -------------------------------------------------------->
+// VARIABLES:
+//Llamada al DOM
+
+
+
+
+// -------------------------------------------------------->
+// EVENTOS:
+//Evento click en botón -> pinta card
+buttonAskInfo.addEventListener("click", () => { 
+  drawUserInfo(); //Ejecutará la función pintar card
+});
+
+
+
+
+
+// -------------------------------------------------------->
+// FUNCIONES:
+//Entrada: API
+const api = () => {
+    const 
+    //Hacer nueva promesa  
+        //Variable isOk 
+        //Transcurridos x segundos (setTimeOut)
+            //si isOk true resolve data (objeto) Crea el objeto
+            //si isOk false reject error
+}
+
+
+const consultaApi = () =>{
+    //Resolución de la promesa 
+    //Llamar a la funcion api 
+        //.then -> resolve
+        //.catch -> reject
+}
+
+
+
+
+// -------------------------------------------------------->
+// INVOCACIONES:
